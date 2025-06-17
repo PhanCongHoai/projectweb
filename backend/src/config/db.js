7ec7ee7183b3820d79f5020eb3a84ef191fd753d
Thanh Hai <lehai8940@gmail.com>
2025-06-17 09:36:32 +0700

    chỉnh lại .env

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -47,3 +47,10 @@ module.exports = {
   connectDB,
   syncDB,
 };
+
+console.log("👉 ENV CHECK", {
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME
+});
